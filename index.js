79ab892dcccee5e42f40dd90d0ea0fb15002047c
Achhaya Pathak <achhayapathak11@gmail.com>
2023-07-15 21:55:56 +0530

    Selecting a route to perform a swap

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 async function runAutomation() {
     const browser = await puppeteer.launch({ headless: false }); // Launch a visible browser window
     const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768});
     await page.goto('https://swap.defillama.com'); // Go to the website
     await new Promise(r => setTimeout(r, 3000));
 
@@ -13,7 +14,7 @@ async function runAutomation() {
     await new Promise(r => setTimeout(r, 1000));
     await page.waitForSelector('.css-ern9ru'); // Wait for the dropdown input to appear
     await page.focus('.css-ern9ru'); // Set focus on the dropdown input
-    await page.keyboard.type('Arbitrum One'); // Type "Arbitrum One" to select it from the dropdown options
+    await page.keyboard.type('Arbitrum'); // Type "Arbitrum One" to select it from the dropdown options
     await page.keyboard.press('Enter'); // Press the Enter key to confirm the selection
     
     
@@ -30,8 +31,8 @@ async function runAutomation() {
     await page.click('button.css-qjhap'); // Click the button to open the search bar
     await page.type('.chakra-text.css-sys4p8', 'USD Coin'); // Type "USD Coin" in the search bar
     await new Promise(r => setTimeout(r, 1000));
-    const secondOption = await page.$$('.cjxQGj').then(options => options[1]);
-    await secondOption.click();
+    const Options = await page.$$('.cjxQGj');
+    await Options[1].click();
 
 
     // Performing Swap to change to You Buy Menu
@@ -45,6 +46,14 @@ async function runAutomation() {
     await page.type('.chakra-text.css-sys4p8', 'WBTC'); // Type "WBTC" in the search bar
     await new Promise(r => setTimeout(r, 1000));
     await page.click(".cjxQGj");
+    
+    
+    // Select the Second Option from Right Menu
+    await new Promise(r => setTimeout(r, 15 * 1000));
+    await page.waitForSelector('.RouteWrapper');
+    const Routes = await page.$$('.RouteWrapper');
+    await Routes[1].click();
+
 
 }
 
