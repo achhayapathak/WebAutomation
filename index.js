10d1b2b63dcba19bd60ddcc1af46a0988eee833c
Achhaya Pathak <achhayapathak11@gmail.com>
2023-07-15 15:22:46 +0530

    Added code to fill You Sell Input

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ async function runAutomation() {
     const page = await browser.newPage();
     await page.goto('https://swap.defillama.com'); // Go to the website
 
+    // Fill out the form
 
-    browser.close();
+    
+    // To clear the existing 10 value in "You Sell" text input
+    await page.click('.css-79elbk input', {clickCount: 3})
+    // To write 12 in "You Sell" text input
+    await page.type('.css-79elbk input', '12')
+
 }
 
 runAutomation();
